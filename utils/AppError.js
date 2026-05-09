e0dd1f8664967c3a4bcebc81ff15142776f23b77
Dabsia <dabojohnson98@gmail.com>
2026-05-09 09:44:11 +0100

    Maade some changes

--- utils/AppError.js
+++ utils/AppError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.status = statusCode;
-    this.isOperational = true;
+    this.isOperational = true
   }
 }
 
